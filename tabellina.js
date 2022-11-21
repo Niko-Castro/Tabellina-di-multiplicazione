e073f3cee4d0bec2c69e8367eb302ba9da8e208a
Niko <nickocastromilla@gmail.com>
2022-11-21 20:56:35 +0100

    clase mattia

---
+++ tabellina.js
@@ -0,0 +1,10 @@
+alert ("CIAO matematico!");
+
+    var table = prompt ("Inserisce il numero di tabellina che vuoi vedere")
+
+    for (var i=0; i<11; i++) { 
+        document.write ( table + "x" + i + "=" + (table * i) + "<br>" )
+        }
+
+
+    
